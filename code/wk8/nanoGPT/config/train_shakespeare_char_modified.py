83b6e2a67b8c40b1df3a7efb34d08cb89ae8ea6b
N00bcak <depictiger@gmail.com>
2025-07-02 16:09:52 +0800

    parameters were very unrealistic

--- code/wk8/nanoGPT/config/train_shakespeare_char_modified.py
+++ code/wk8/nanoGPT/config/train_shakespeare_char_modified.py
@@ -4,7 +4,7 @@
 out_dir = 'out-shakespeare-char'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
-log_interval = 10 # don't print too too often
+log_interval = 25 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
@@ -16,17 +16,17 @@ wandb_run_name = 'mini-gpt'
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
 batch_size = 32 # Suppress batch size since our GPU is a baby, and we would like noisier gradients
-block_size = 256 # context of up to 256 previous characters
+block_size = 512 # context of up to 512 previous characters
 
 # Longer, thinner model for greater expressiveness
-n_layer = 8
+n_layer = 12
 n_head = 8
 n_embd = 256
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 20000
-lr_decay_iters = 20000 # make equal to max_iters usually
+max_iters = 5000
+lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
